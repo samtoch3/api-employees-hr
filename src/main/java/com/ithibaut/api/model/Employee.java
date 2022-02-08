c80780c3d78316a23eb1302644cda44e15b3f2ef
Samson N. <samson.nzumgueng@outlook.com>
2022-02-08 17:23:06 +0100

    Initial files

---
+++ src/main/java/com/ithibaut/api/model/Employee.java
@@ -0,0 +1,75 @@
+package com.ithibaut.api.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="mail")
+    private String mail;
+
+    @Column(name="password")
+    private String password;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+    
+    
+    
+}
---
+++ src/main/java/com/ithibaut/api/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.ithibaut.api.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ithibaut.api.model.Employee;
+
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+}
